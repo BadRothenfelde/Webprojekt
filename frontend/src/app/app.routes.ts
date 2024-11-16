0f09b803635e71cdf39f3d3c8f7b4d5d5657631e
BadRothenfelde <dizzyrascalger@gmail.com>
2024-11-16 13:09:13 +0100

    Imprint & Shopping Cart

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from "./landing/landingpage.component"
 import { AboutComponent } from "./about/about.component"
+import { ImprintComponent} from "./imprint/imprint.component"
 
 export const routes: Routes = [
     {path: "home", component: LandingComponent },
     {path: "about", component: AboutComponent },
+    {path: "imprint", component: ImprintComponent },
     {path: "", redirectTo: "/home", pathMatch: "full" }
 ];
 
