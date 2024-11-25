bc2b62e945cbdbba0bcf65713b626456b059a53b
daniel <dizzyrascalger@gmail.com>
2024-11-25 14:50:00 +0100

    Added Allergenes feature

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { LandingComponent } from "./landing/landingpage.component"
 import { AboutComponent } from "./about/about.component"
 import { ImprintComponent} from "./imprint/imprint.component"
 import { MenuComponent } from './menu/menu.components';
+import { AllergyComponent } from './allergy/allergy.component';
 
 export const routes: Routes = [
     {path: "home", component: LandingComponent },
     {path: "about", component: AboutComponent },
     {path: "imprint", component: ImprintComponent },
     {path: "menu", component: MenuComponent},
+    {path: "allergy", component: AllergyComponent },
     {path: "", redirectTo: "/home", pathMatch: "full" }
 ];
 
